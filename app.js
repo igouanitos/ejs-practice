12a6b68a28232e277780b80a3e372db7cca39b2a
igouanitos <siamourelis@gmail.com>
2023-07-20 14:42:14 +0300

    finished
    the full project. v1 is done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const home = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,17 +17,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//test
 app.get('/posts/:postId', (req, res) => {
   var check = false;
   var postToRender = Object;
   posts.forEach((i)=>{
-    if (i.title == req.params.postId){
+    if (_.lowerCase(i.title) == _.lowerCase(req.params.postId)){
       check = true;
       postToRender = i;
     }
   })
-
+// _.kebabCase
+//_.lowerCase
   if (check = true){
     res.render('post', { content : postToRender} )
   } else{
@@ -34,9 +35,9 @@ app.get('/posts/:postId', (req, res) => {
       title : 404,
       body : "Couldn't find the url you are looking for."
     }
+    res.render('post', { content : errorMsg} )
+
   }
-  
-   
 
 })
 
